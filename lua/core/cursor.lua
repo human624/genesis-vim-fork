e42ac25e438b943ad2e213854ee6ac9799c6c3cf
human624 <htr654509@gmail.com>
2026-01-01 19:14:23 +0500

    Add cursor position restoration on file open
    This script saves the cursor position when a file is opened in Neovim.

---
+++ lua/core/cursor.lua
@@ -0,0 +1,12 @@
+-- cursor.lua: запоминаем позицию курсора при открытии файла
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      vim.api.nvim_win_set_cursor(0, {mark[1], mark[2]})
+    end
+  end,
+})
+

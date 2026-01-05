c81dfcd5a3c8371be18a4a0b7c205343cdbedfc0
human624 <htr654509@gmail.com>
2026-01-05 12:55:39 +0500

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,30 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "json5",
+        "lua",
+        "python",
+        "vim",
+        "yaml",
+        "c",
+        "go",
+        "rust",
+      },
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-	"bash",
-	"css",
-	"dockerfile",
-	"html",
-	"javascript",
-	"json",
-	"json5",
-	"lua",
-	"python",
-	"vim",
-	"yaml",
-	"c",
-	"go",
-	"rust",
-	},
+      sync_install = false,
+      auto_install = true,
 
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
 }
